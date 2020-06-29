bfd71dd0f14247a8f462bcfd96735802a0749be4
vzdrizhni <vzdrizhni@gmail.com>
2020-06-29 15:59:37 +0500

    Build html structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import './scss/style.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,39 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-   rules: [
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-    ],
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
 };
